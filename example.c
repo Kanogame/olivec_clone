6a6f07785e8d627156b523af4e7f2a9216c4db98
kanogame <wasdqert1222@gmail.com>
2023-07-27 16:52:02 +0700

    circle added

--- example.c
+++ example.c
@@ -17,6 +17,26 @@ static uint32_t pixels[HEIGHT*WIDTH];
 #define CELL_HEIGHT (HEIGHT/ROWS)
 #define BACKGROUND_COLOR 0xFF202020;
 
+void olivec_fill_circle(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int cx, int cy, int r, uint32_t color) {
+    int x1 = cx -r;
+    int y1 = cy -r;
+    int x2 = cx +r;
+    int y2 = cy +r;
+    for (int y = y1; y <= y2; y++) {
+        if (0 <= y && y < (int) pixels_height) {
+            for (int x = x1; x <= x2; x++) {
+                if (0 <= x && x < (int) pixels_width) {
+                    int dx = x -cx;
+                    int dy = y -cy;
+                    if (dx*dx + dy*dy <= r*r) {
+                        pixels[y*pixels_width + x] = color;
+                    }
+                }
+            }
+        }
+    }
+}
+
 bool checker_example() {
     olivec_fill(pixels, WIDTH, HEIGHT, 0xFF202020);
 
@@ -31,7 +51,7 @@ bool checker_example() {
             olivec_fill_rect(pixels, WIDTH, HEIGHT, x*CELL_WIDTH, y*CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, color);
         }
     }
-    const char *file_path = "checker_example.ppm";
+    const char *file_path = "checker.ppm";
     Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
     if (err) {
         fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
@@ -41,7 +61,22 @@ bool checker_example() {
     return true;
 }
 
+bool circle_example() {
+    olivec_fill(pixels, WIDTH, HEIGHT, 0xFF202020);
+
+    olivec_fill_circle(pixels, WIDTH, HEIGHT, WIDTH /2, HEIGHT / 2, 100, 0xFF2020FF);
+
+    const char *file_path = "circle.ppm";
+    Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+    if (err) {
+        fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
+        return false;
+    }
+    return true;
+}
+
 int main(void) {
-    checker_example();
+    if (!checker_example()) return -1;
+    if (!circle_example()) return -1;
     return 0;
 }
