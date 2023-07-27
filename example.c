368800de615c8761e4183938b259b815cd2288fd
kanogame <wasdqert1222@gmail.com>
2023-07-27 16:17:32 +0700

    rectangle added

--- example.c
+++ example.c
@@ -1,53 +1,37 @@
-#include "olive.c"
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
+#include <string.h>
+#include "olive.c"
 
 #define WIDTH 800
 #define HEIGHT 600
 
-void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color) {
-    for (size_t i =0; i < width*height; i++) {
-        pixels[i] = color;
-    }
-}
-
-typedef int Errno;
-
-#define return_defer(value) do {result = (value); goto defer;} while (0)
-
-Errno olivec_save_to_ppm_file(uint32_t *pixels, size_t width, size_t height, const char *file_path) {
-    int result = 0;
-    FILE *f = NULL;
-    {
-        FILE *f = fopen(file_path, "wb");
-        if (f == NULL)return_defer(errno); 
-
-        fprintf(f, "P6\n%zu %zu\n255\n", width, height);
-        if (ferror(f))return_defer(errno); 
-
-        for (size_t i =0 ; i < width*height; i++) {
-            uint32_t pixel = pixels[i];
-            uint8_t bytes[3] = {
-                (pixel>>8*0)&0xFF,
-                (pixel>>8*1)&0xFF,
-                (pixel>>8*2)&0xFF,
-            };
-            fwrite(bytes, sizeof(bytes), 1, f);
-            if (ferror(f)) return_defer(errno);
+void olivec_fill_rect(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int x0, int y0, size_t w, size_t h, uint32_t color) {
+    for (int dy = 0; dy <(int) h; dy++) {
+        int y = y0 + dy;
+        if (0 <= y && y < (int)pixels_height) {
+            for (int dx = 0; dx < (int) w; dx++) {
+                int x = x0 + dx;
+                if (0 <= x && x < (int) pixels_width) {
+                    pixels[y*pixels_width + x] = color;
+                }
+            }
         }
     }
-
-defer:
-    if (f) fclose(f);
-    return result;
 }
 
 static uint32_t pixels[HEIGHT*WIDTH];
 
 int main(void) {
-    olivec_fill(pixels, WIDTH, HEIGHT, 0xFF0000FF);
-    int err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, "output.ppm");
-    printf("%i", err);
+    olivec_fill(pixels, WIDTH, HEIGHT, 0xFF202020);
+    size_t rw = 50 *4;
+    size_t rh = 30 * 4;
+    olivec_fill_rect(pixels, WIDTH, HEIGHT, WIDTH/2 - rw/2, HEIGHT/2 - rh/2, rw, rh, 0xFF2020FF);
+    const char *file_path = "output.ppm";
+    Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+    if (err) {
+        fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
+    }
     return 0;
 }
