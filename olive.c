727f1b6fe7c743422554a84cfcdb4c82b5ddac14
kanogame <wasdqert1222@gmail.com>
2023-07-28 16:46:20 +0700

    sort_triangle_points_by_y added

--- olive.c
+++ olive.c
@@ -1,6 +1,12 @@
 #ifndef OLIVE_C_
 #define OLIVE_C_
 
+void swap_int(int *value1, int *value2) {
+    int mem = *value1;
+    *value1 = *value2;
+    *value2 = mem;
+}
+
 void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color) {
     for (size_t i =0; i < width*height; i++) {
         pixels[i] = color;
@@ -42,6 +48,58 @@ void olivec_fill_circle(uint32_t *pixels, size_t pixels_width, size_t pixels_hei
     }
 }
 
+void olivec_draw_line(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int x1, int y1, int x2, int y2, uint32_t color) {
+    int dx = x2 -x1;
+    int dy = y2 -y1;
+
+    if (dx != 0) {
+        int k = dy/dx;
+        int c = y1 - dy*x1/dx;
+
+        if (x1 > x2) swap_int(&x1, &x2);
+        for (int x = x1; x <= x2; x++) {
+            if (0 <= x && x < (int) pixels_width) {
+                int sy1 = dy*x/dx +c;
+                int sy2 = dy*(x + 1)/dx +c;
+                if (sy1 > sy2) swap_int(&sy1, &sy2);
+                for (int y = sy1; y <= sy2; y++) {
+                    if (0 <= y && y < (int) pixels_height) {
+                        pixels[y*pixels_width + x] = color;
+                    }
+                }
+            }
+        }
+    } else {
+        int x = x1;
+        if (0 <= x && x < (int) pixels_width) {
+            if (y1 < y2) swap_int(&y1, &y2);
+            for (int y = y1; y <= y1; y++) {
+                if (0 <= y && y < (int) pixels_height) {
+                    pixels[y*pixels_width +x] = color;
+                }
+            }
+        }
+    }
+}
+
+void sort_triangle_points_by_y(int *x1, int *y1, int *x2, int *y2, int *x3, int *y3) {
+    if (*y1 > *y2) {
+        swap_int(*y1, *y2);
+        swap_int(*x1, *x2);
+    }
+    if (*y2 > *y3) {
+        swap_int(*y2, *y3);
+        swap_int(*x2, *x3);
+    }
+    if (*y1 > *y2) {
+        swap_int(*y1, *y2);
+        swap_int(*x1, *x2);
+    }
+} 
+
+void olivec_fill_triangle(uint32_t *pixels, size_t width, size_t height, int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color) {
+
+}
 
 typedef int Errno;
 
